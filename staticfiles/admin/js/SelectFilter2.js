9b9f90b4afdc66cece7b915acc47517cf97a2d18
PolinaMiteva <polly2706@gmail.com>
2020-11-24 22:01:54 +0200

    heroku prep 3

--- staticfiles/admin/js/SelectFilter2.js
+++ staticfiles/admin/js/SelectFilter2.js
@@ -2,7 +2,7 @@
 /*
 SelectFilter2 - Turns a multiple-select box into a filter interface.
 
-Requires core.js and SelectBox.js.
+Requires Django_fnl_project.js and SelectBox.js.
 */
 'use strict';
 {
