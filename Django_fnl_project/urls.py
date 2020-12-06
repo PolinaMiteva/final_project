4e52cec065367b67ce3b5bdd4b796f00a85f9a26
PolinaMiteva <polly2706@gmail.com>
2020-12-06 20:23:30 +0200

    three info pages and updated css

--- Django_fnl_project/urls.py
+++ Django_fnl_project/urls.py
@@ -2,27 +2,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from Django_fnl_project import views as main_views
-# from authentication import views as authentication_views
-# from django.contrib.auth import views as contrib_auth_views
 from Django_fnl_project import settings
 
 urlpatterns = [
     path('', main_views.index, name='index'),
+    path('practice/', main_views.practice, name='the-practice'),
+    path('nrg-cleansing/', main_views.cleansing, name='nrg-cleansing'),
+    path('nrg-massage/', main_views.massage, name='nrg-massage'),
     path('admin/', admin.site.urls),
     path('profile/', include('user_profile.urls')),
     path('', include('authentication.urls')),
     path('blog/', include('blog.urls')),
 ]
-
-# urlpatterns = [
-#     path('', main_views.index, name='index'),
-#     path('admin/', admin.site.urls),
-#     path('register/', authentication_views.register, name='register'),
-#     path('login/', contrib_auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-#     path('logout/', authentication_views.logout_user, name='logout'),
-#     path('profile/', include('user_profile.urls')),
-# ]
-
 
 
 if settings.DEBUG:
--- Django_fnl_project/views.py
+++ Django_fnl_project/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def practice(request):
+    return render(request, 'the_practice.html')
+
+
+def cleansing(request):
+    return render(request, 'nrg_cleansing.html')
+
+
+def massage(request):
+    return render(request, 'nrg-massage.html')
